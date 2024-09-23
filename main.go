f8fce6a7cfc59451a30fdd537fe5224541122574
Yanis Benyacine <sterllytb@gmail.com>
2024-09-23 10:33:16 +0200

    add Game-State

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"container/list"
-	"os"
 	"fmt"
+	"os"
 )
+
+type STATE int64
+
+const (
+	WAITING STATE = iota
+	PLAYING
+	END
+)
+
+var GAME_STATE STATE
 
 func main() {
 	fmt.Print("f")
+	list.New()
+	os.Exit(0)
 }
